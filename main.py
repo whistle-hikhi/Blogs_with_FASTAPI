816eeabc921bd73daa5e7952e634750f5c95db11
Hiro Nguyen <hiro.nguyen@techvify.com.vn>
2024-12-18 00:18:06 +0700

    CRUD

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from fastapi import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
-@app.get("/")
+class Blog(BaseModel):
+    title: str
+    content: str
+    published: bool=True
+    score: Optional[int]=None
+
+@app.get("/login")
 def read_book():
-    return {"message": "Hello World"}
+    return {"message": "Yahalo"}
+
+@app.get("/blogs")
+def get_blogs():
+    return {"data": "This is a blog of you"}
+
+@app.post("/make_blogs")
+def make_blogs(blog: Blog):
+    return {"message": blog.model_dump()}
